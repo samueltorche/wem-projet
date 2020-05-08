ed69fac8df16f157b4edde0fff1cb7ff443c4bf4
Samuel Torche <samuel.torche@edu.hefr.ch>
2020-05-08 07:21:13 +0200

    a priori on small subset

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from mlxtend.frequent_patterns import apriori
+# from mlxtend.frequent_patterns import apriori
+from apyori import apriori
 from mlxtend.frequent_patterns import association_rules
 
 data_movies = pd.read_csv('movies.csv')
@@ -18,9 +19,41 @@ print('this dataset contains: ', len(data_movies), 'movies')
 data_ratings = pd.read_csv('ratings.csv', usecols=[0, 1])
 print('this dataset contains: ', len(data_ratings), 'ratings')
 
-data_ratings = data_ratings.groupby(['userId','movieId']).size().reset_index(name='count')
+limit_testing = 100000
+df = data_ratings[:limit_testing].groupby(['userId','movieId']).size().reset_index(name='count')
 
-print(len(data_ratings))
-basket = data_ratings.groupby(['userId', 'movieId'])['count'].sum().unstack().reset_index().fillna(0).set_index('userId')
-print(len(basket))
-basket.head(10)
+print("Ratings effectively taken:", len(df))
+
+basket = df.groupby(['userId', 'movieId'])['count'].sum().unstack().reset_index().fillna(0).set_index('userId')
+
+#The encoding function
+def encode_units(x):
+    if x <= 0:
+        return 0
+    if x >= 1:
+        return 1
+
+basket_sets = basket.applymap(encode_units)
+
+def mlxtend_mba(basket_sets):
+   frequent_itemsets = apriori(basket_sets, min_support=0.1, use_colnames=True)
+   rules = association_rules(frequent_itemsets, metric="lift")
+   rules.sort_values('confidence', ascending = False, inplace = True)
+   # rules.head(10)
+   print(rules)
+   
+   
+def apyori_mba(basket_sets, basket_len, basket_wid):
+   transactions = []
+   for i in range(0, basket_len):
+      transactions.append([str(basket_sets.values[i,j]) for j in range(0, basket_wid)])
+   rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+   print(rules)
+
+
+print("Number of transactions", len(basket_sets))
+print("Number of movies", len(basket_sets.columns))   
+# mlxtend_mba(basket_sets)
+apyori_mba(basket_sets, len(basket_sets), len(basket_sets.columns))
+
+print("------- END ---------")
